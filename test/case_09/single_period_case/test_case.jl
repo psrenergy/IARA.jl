e6dcb0e0e14b284306044c7bedb8c1d157acaebe
Gabriel Vidigal <gvidigal@psr-inc.com>
2025-01-23 18:41:01 -0300

    fix single period tests

--- test/case_09/single_period_case/test_case.jl
+++ test/case_09/single_period_case/test_case.jl
@@ -29,30 +29,30 @@ output_path = joinpath(PATH, "outputs")
 IARA.single_period_market_clearing(
     PATH;
     period = 1,
-    output_path = joinpath(PATH, "period_1"),
+    output_path = joinpath(output_path, "period_1"),
     plot_outputs = false,
     delete_output_folder_before_execution = true,
 )
-Main.copy_files(joinpath(PATH, "period_1"), PATH, x -> contains(x, ".json"))
+Main.copy_files(joinpath(output_path, "period_1"), PATH, x -> contains(x, ".json"))
 IARA.single_period_market_clearing(
     PATH;
     period = 2,
-    output_path = joinpath(PATH, "period_2"),
+    output_path = joinpath(output_path, "period_2"),
     plot_outputs = false,
     delete_output_folder_before_execution = true,
 )
-Main.copy_files(joinpath(PATH, "period_2"), PATH, x -> contains(x, ".json"))
+Main.copy_files(joinpath(output_path, "period_2"), PATH, x -> contains(x, ".json"))
 IARA.single_period_market_clearing(
     PATH;
     period = 3,
-    output_path = joinpath(PATH, "period_3"),
+    output_path = joinpath(output_path, "period_3"),
     plot_outputs = false,
     delete_output_folder_before_execution = true,
 )
 
-Main.send_files(joinpath(PATH, "period_1"), output_path)
-Main.send_files(joinpath(PATH, "period_2"), output_path)
-Main.send_files(joinpath(PATH, "period_3"), output_path)
+Main.send_files(joinpath(output_path, "period_1"), output_path)
+Main.send_files(joinpath(output_path, "period_2"), output_path)
+Main.send_files(joinpath(output_path, "period_3"), output_path)
 
 if Main.UPDATE_RESULTS
     Main.update_outputs!(PATH)

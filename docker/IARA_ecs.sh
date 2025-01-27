72fd34de52e83fed4ea4e6b76f944e934210e859
fellipessanha <fellipe900@gmail.com>
2025-01-27 16:23:36 -0300

    fix: call sh functions correctly

--- docker/IARA_ecs.sh
+++ docker/IARA_ecs.sh
@@ -28,7 +28,7 @@ CASE_PATH="iara-case"
 
 function download_and_unzip_case () {
     echo "Downloading input data..."
-    aws s3 cp s3://$S3_BUCKET/games/$IARA_CASE.zip ./$IARA_CASE.zip > /dev/null 2>&1
+    aws s3 cp s3://$S3_BUCKET/games/$IARA_CASE/game_inputs.zip ./$IARA_CASE.zip > /dev/null 2>&1
     unzip -qo $IARA_CASE.zip -d $CASE_PATH
     echo "Completed."
 }
@@ -66,9 +66,9 @@ fi
 # Generate json and htmls for case creation
 if [ "$IARA_COMMAND" == "json and htmls for case creation" ]; then
 
-    download_and_unzip_case()
+    download_and_unzip_case
 
-    $IARA_PATH/IARA_interface_call.sh --output-path 'game_summary'  --run-mode 'min-cost' $CASE_PATH 
+    $IARA_PATH/IARA_interface_call.sh --output-path $CASE_PATH/game_summary  --run-mode 'min-cost' $CASE_PATH 
     
     echo "Uploading results to S3..."
     aws s3 cp ./$CASE_PATH/game_summary/ s3://$S3_BUCKET/games/$IARA_CASE/game_summary/ --recursive > /dev/null 2>&1
@@ -78,10 +78,10 @@ fi
 
 # Generate heuristic bid
 if [ "$IARA_COMMAND" == "heuristic bid" ]; then
-    validate_game_round()
-    download_and_unzip_case()
+    validate_game_round
+    download_and_unzip_case
 
-    $IARA_PATH/IARA.sh $CASE_PATH --ouput-path 'heuristic_bids' --run-mode 'single-period-heuristic-bid' --period $GAME_PERIOD # TODO missing other arguments
+    $IARA_PATH/IARA.sh $CASE_PATH --output-path $CASE_PATH/heuristic_bids --run-mode 'single-period-heuristic-bid' --period $GAME_PERIOD # TODO missing other arguments
     
     echo "Uploading results to S3..."
     aws s3 cp ./$CASE_PATH/heuristic_bids/ s3://$S3_BUCKET/games/$IARA_CASE/game_round_$IARA_GAME_ROUND/heuristic_bids/ --recursive > /dev/null 2>&1
@@ -91,8 +91,8 @@ fi
 
 # Run single period market clearing
 if [ "$IARA_COMMAND" == "single period market clearing" ]; then
-    validate_game_round()
-    download_and_unzip_case()
+    validate_game_round
+    download_and_unzip_case
 
     $IARA_PATH/IARA.sh $CASE_PATH # TODO missing other arguments
     

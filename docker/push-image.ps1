4094b957273b90f4ce61f820e36e0e31d2c632eb
pedroripper <pedros.ripper@gmail.com>
2025-01-29 11:44:06 -0300

    Fix [no ci]

--- docker/push-image.ps1
+++ docker/push-image.ps1
@@ -1,3 +1,3 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $Env:AWS_ACCOUNT_ID
-docker build -t $Env:AWS_ACCOUNT_ID/iara:latest .
-docker push $Env:AWS_ACCOUNT_ID/iara:latest
+aws ecr get-login-password --region $Env:AWS_REGION | docker login --username AWS --password-stdin $Env:AWS_ACCOUNT_ID + ".dkr.ecr." + $Env:AWS_REGION + ".amazonaws.com"
+docker build -t $Env:AWS_ACCOUNT_ID + ".dkr.ecr." + $Env:AWS_REGION + ".amazonaws.com/iara:latest"
+docker push $Env:AWS_ACCOUNT_ID + ".dkr.ecr." + $Env:AWS_REGION + ".amazonaws.com/iara:latest"

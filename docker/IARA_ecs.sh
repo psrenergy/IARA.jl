b9a76d45d06e14e82412ee67d6c903bc07ede387
pedroripper <pedros.ripper@gmail.com>
2025-01-29 10:31:53 -0300

    Minor fix [no ci]

--- docker/IARA_ecs.sh
+++ docker/IARA_ecs.sh
@@ -16,6 +16,8 @@
 #
 # $IARA_GAME_ROUND: The round to be executed. It should be an integer
 # 
+# $GAME_PERIOD: The period to be executed. It should be an integer
+#
 # Usage:
 #  $ ./IARA_ecs.sh
 
@@ -94,7 +96,7 @@ if [ "$IARA_COMMAND" == "single period market clearing" ]; then
     validate_game_round
     download_and_unzip_case
 
-    $IARA_PATH/IARA.sh $CASE_PATH # TODO missing other arguments
+    $IARA_PATH/IARA.sh $CASE_PATH --run-mode 'single-period-market-clearing' --period $GAME_PERIOD # TODO missing other arguments
     
     echo "Uploading results to S3..."
     aws s3 cp ./$CASE_PATH/outputs/ s3://$S3_BUCKET/games/$IARA_CASE/game_round_$IARA_GAME_ROUND/results/ --recursive > /dev/null 2>&1

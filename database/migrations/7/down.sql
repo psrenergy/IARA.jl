d04f402a4122294de60dbfe0deee8fbc41998884
Gabriel Vidigal <gvidigal@psr-inc.com>
2025-04-14 11:46:47 -0300

    Update down migration

--- database/migrations/7/down.sql
+++ database/migrations/7/down.sql
@@ -1,5 +1,8 @@
 PRAGMA user_version = 6;
 PRAGMA foreign_keys = ON;
 
-ALTER TABLE Configuration DROP COLUMN train_mincost_time_limit_sec;
-ALTER TABLE Configuration RENAME COLUMN train_mincost_iteration_limit TO iteration_limit;
+ALTER TABLE Configuration_vector_expected_number_of_repeats_per_node ADD COLUMN temp_expected_number_of_repeats_per_node INT;
+UPDATE Configuration_vector_expected_number_of_repeats_per_node SET temp_expected_number_of_repeats_per_node = expected_number_of_repeats_per_node;
+ALTER TABLE Configuration_vector_expected_number_of_repeats_per_node DROP COLUMN expected_number_of_repeats_per_node;
+ALTER TABLE Configuration_vector_expected_number_of_repeats_per_node RENAME COLUMN temp_expected_number_of_repeats_per_node TO expected_number_of_repeats_per_node;
+

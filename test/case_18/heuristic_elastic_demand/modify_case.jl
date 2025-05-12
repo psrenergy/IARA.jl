cde6667d2740afa1f9a328e419469d3aa683921b
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-05-12 15:02:54 -0300

    add heuristic bid demand offer case

---
+++ test/case_18/heuristic_elastic_demand/modify_case.jl
@@ -0,0 +1,81 @@
+#  Copyright (c) 2024: PSR, CCEE (Câmara de Comercialização de Energia  
+#      Elétrica), and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#############################################################################
+# IARA
+# See https://github.com/psrenergy/IARA.jl
+#############################################################################
+
+# Remove offer files if they exist
+files_to_remove = [
+    "price_offer.csv",
+    "price_offer.toml",
+    "quantity_offer.csv",
+    "quantity_offer.toml",
+]
+
+for file in files_to_remove
+    file_path = joinpath(@__DIR__, file)
+    if isfile(file_path)
+        rm(file_path)
+        @info "Removed file: $file_path"
+    end
+end
+
+db = IARA.load_study(PATH; read_only = false)
+
+IARA.update_configuration!(db;
+    bid_data_source = IARA.Configurations_BidDataSource.PRICETAKER_HEURISTICS,
+)
+
+IARA.update_bidding_group!(
+    db,
+    "bg_1";
+    bid_type = IARA.BiddingGroup_BidType.MARKUP_HEURISTIC,
+)
+
+IARA.update_bidding_group_vectors!(
+    db,
+    "bg_1";
+    risk_factor = [0.1],
+    segment_fraction = [1.0],
+)
+
+IARA.update_bidding_group!(
+    db,
+    "bg_2";
+    bid_type = IARA.BiddingGroup_BidType.MARKUP_HEURISTIC,
+)
+
+IARA.update_bidding_group_vectors!(
+    db,
+    "bg_2";
+    risk_factor = [0.2, 0.3],
+    segment_fraction = [0.4, 0.6],
+)
+
+# Create the demand price timeseries
+demand_price = zeros(1, number_of_subperiods, number_of_scenarios, number_of_periods)
+demand_price[1, 1, :, :] .= 0.3
+demand_price[1, 2, :, :] .= 1.3
+
+IARA.write_timeseries_file(
+    joinpath(PATH, "demand_price"),
+    demand_price;
+    dimensions = ["period", "scenario", "subperiod"],
+    labels = ["dem_2"],
+    time_dimension = "period",
+    dimension_size = [number_of_periods, number_of_scenarios, number_of_subperiods],
+    initial_date = "2020-01-01",
+    unit = "\$/MWh",
+)
+
+IARA.link_time_series_to_file(
+    db,
+    "DemandUnit";
+    elastic_demand_price = "demand_price",
+)
+
+IARA.close_study!(db)

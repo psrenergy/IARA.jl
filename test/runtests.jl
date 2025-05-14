feb3a8260a8404f2ff22711a4530896138d15bca
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-05-14 11:31:10 -0300

    add missing cases in runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -140,6 +140,17 @@ reduced_test_list = Dict(
 #     "case_15/base_case/test_case.jl",
 #     "case_15/renewable/test_case.jl",
 # ],
+# "case_16" => [
+#     "case_16/base_case/test_case.jl",
+#     "case_16/market_fcf_tiebeaker/test_case.jl",
+# ],
+# "case_17" => [
+#     "case_17/base_case/test_case.jl",
+# ],
+# "case_18" => [
+#     "case_18/base_case/test_case.jl",
+#     "case_18/heuristic_elastic_demand/test_case.jl",
+# ],
 )
 
 test_list = isempty(reduced_test_list) ? test_modules(@__DIR__) : reduced_test_list

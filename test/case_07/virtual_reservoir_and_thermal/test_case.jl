21aee1a7b98d5a6417b5e2d8f3e5669a89de90a9
carolmcs <monteirocarneirosc@gmail.com>
2025-06-05 14:28:42 -0300

    Update test

--- test/case_07/virtual_reservoir_and_thermal/test_case.jl
+++ test/case_07/virtual_reservoir_and_thermal/test_case.jl
@@ -8,6 +8,7 @@ const PATH = @__DIR__
 db = nothing
 try
     include("../base_case/build_case.jl")
+    include("../reference_curve_case/modify_case.jl")
     include("modify_case.jl")
 finally
     if db !== nothing

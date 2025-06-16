bde9b9c169fce908c674a2b4ee907c26c83ee1de
Gabriel Vidigal <gvidigal@psr-inc.com>
2025-06-15 23:04:07 -0300

    Add migration after merge

---
+++ database/migrations/14/up.sql
@@ -0,0 +1,7 @@
+PRAGMA user_version = 14;
+PRAGMA foreign_keys = ON;
+
+DROP TABLE Configuration_vector_reference_curve_demand_multipliers;
+
+ALTER TABLE Configuration ADD COLUMN reference_curve_number_of_segments INTEGER NOT NULL DEFAULT 10;
+ALTER TABLE Configuration ADD COLUMN reference_curve_final_segment_price_markup REAL NOT NULL DEFAULT 0.01;

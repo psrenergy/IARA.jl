0f2432f21eb914899dd66882b23f59755aebf7a5
Gabriel Vidigal <gvidigal@psr-inc.com>
2025-06-25 14:44:04 -0300

    [no ci] remame migration

--- database/migrations/17/down.sql
+++ database/migrations/17/down.sql
@@ -1,4 +1,4 @@
-PRAGMA user_version = 15;
+PRAGMA user_version = 16;
 PRAGMA foreign_keys = ON;
 
 ALTER TABLE BiddingGroup DROP COLUMN fixed_cost;
--- database/migrations/17/up.sql
+++ database/migrations/17/up.sql
@@ -1,4 +1,4 @@
-PRAGMA user_version = 16;
+PRAGMA user_version = 17;
 PRAGMA foreign_keys = ON;
 
 ALTER TABLE BiddingGroup ADD COLUMN fixed_cost REAL NOT NULL DEFAULT 0.0;

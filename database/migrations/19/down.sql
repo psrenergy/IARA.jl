5aa41d4ca18c18b33ecf2d6866888a0e0dcd1e29
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-07-18 11:05:01 -0300

    fix merge

--- database/migrations/19/down.sql
+++ database/migrations/19/down.sql
@@ -1,7 +1,21 @@
 PRAGMA user_version = 18;
 PRAGMA foreign_keys = ON;
 
-ALTER TABLE DemandUnit RENAME COLUMN curtailment_cost_flexible_demand TO curtailment_cost;
-ALTER TABLE DemandUnit RENAME COLUMN max_shift_up_flexible_demand TO max_shift_up;
-ALTER TABLE DemandUnit RENAME COLUMN max_shift_down_flexible_demand TO max_shift_down;
-ALTER TABLE DemandUnit RENAME COLUMN max_curtailment_flexible_demand TO max_curtailment;
+ALTER TABLE Configuration ADD COLUMN price_limits INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE Configuration RENAME COLUMN bid_data_processing TO bid_data_source;
+UPDATE Configuration SET bid_data_source = CASE
+    WHEN bid_data_source = 0 THEN 0
+    WHEN bid_data_source = 1 THEN 0
+    WHEN bid_data_source = 2 THEN 1
+    WHEN bid_data_source = 3 THEN 1
+    ELSE 0
+END;
+ALTER TABLE Configuration ADD COLUMN bidding_group_bid_validation INTEGER;
+UPDATE Configuration SET bidding_group_bid_validation = CASE
+    WHEN bid_data_source = 0 THEN 0
+    WHEN bid_data_source = 1 THEN 1
+    WHEN bid_data_source = 2 THEN 0
+    WHEN bid_data_source = 3 THEN 1
+    ELSE 0
+END;
+UPDATE Configuration SET make_whole_payments = 0;

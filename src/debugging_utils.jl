b67fc0bcd720704ba32c966bea1c4a5bd815498f
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-08-01 12:27:12 -0300

    fix lp write names

--- src/debugging_utils.jl
+++ src/debugging_utils.jl
@@ -17,16 +17,18 @@ function lp_filename(inputs::Inputs, run_time_options::RunTimeOptions, t::Intege
         return joinpath(path_case(inputs), "t$(t)_s$(scen)_reference_curve.lp")
     elseif run_mode(inputs) == RunMode.TRAIN_MIN_COST
         return joinpath(path_case(inputs), "t$(t)_s$(scen)_train_min_cost.lp")
-    elseif iterate_nash_equilibrium(inputs)
-        return joinpath(path_case(inputs), "t$(t)_s$(scen)_a$(run_time_options.asset_owner_index).lp")
+    elseif iterate_nash_equilibrium(inputs) && is_bidder(inputs, run_time_options)
+        return joinpath(path_case(inputs), "NASH_ITERATION_" * string(nash_equilibrium_iteration(inputs, run_time_options)) * "_t$(t)_s$(scen)_a$(run_time_options.asset_owner_index).lp")
     else # Clearing
+        nash_eq_iteration = nash_equilibrium_iteration(inputs, run_time_options)
+        nash_eq_string = nash_eq_iteration > 0 ? "NASH_ITERATION_$(nash_eq_iteration)_" : ""
         if is_ex_post_problem(run_time_options)
             return joinpath(
                 path_case(inputs),
-                "$(run_time_options.clearing_model_subproblem)_t$(t)_s$(scen)_ss$(subscenario).lp",
+                nash_eq_string * "$(run_time_options.clearing_model_subproblem)_t$(t)_s$(scen)_ss$(subscenario).lp",
             )
         else
-            return joinpath(path_case(inputs), "$(run_time_options.clearing_model_subproblem)_t$(t)_s$(scen).lp")
+            return joinpath(path_case(inputs), nash_eq_string * "$(run_time_options.clearing_model_subproblem)_t$(t)_s$(scen).lp")
         end
     end
 end

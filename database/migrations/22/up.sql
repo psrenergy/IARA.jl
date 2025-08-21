88209f3a0b2e6ec2f242de25c1157651110203be
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-08-21 14:51:18 -0300

    fix merge (again)

--- database/migrations/22/up.sql
+++ database/migrations/22/up.sql
@@ -14,3 +14,5 @@ INSERT INTO AssetOwner_vector_purchase_discount_rate (id, vector_index, purchase
     1 AS vector_index,
     purchase_discount_rate AS purchase_discount_rate
 FROM AssetOwner WHERE purchase_discount_rate IS NOT NULL;
+
+ALTER TABLE AssetOwner DROP COLUMN purchase_discount_rate;

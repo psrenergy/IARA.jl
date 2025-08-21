41dce25343c1693406d0b4989bc0d0bf13b5838d
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-08-21 15:21:18 -0300

    fix format

--- src/bids.jl
+++ src/bids.jl
@@ -730,7 +730,7 @@ function must_read_hydro_unit_data_for_markup_wizard(
     run_time_options::RunTimeOptions = RunTimeOptions(),
 )
     # Run mode
-    if run_mode(inputs) == RunMode.TRAIN_MIN_COST || run_mode(inputs) == RunMode.MIN_COST 
+    if run_mode(inputs) == RunMode.TRAIN_MIN_COST || run_mode(inputs) == RunMode.MIN_COST
         return false
     end
     if !nash_equilibrium_initialization_run_time(inputs, run_time_options) && iterate_nash_equilibrium(inputs)

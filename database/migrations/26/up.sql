559496faa6f86cf9d5a33ed92116fd6476324018
Rafael Benchimol Klausner <rafa.bench@gmail.com>
2025-09-18 13:46:59 -0300

    fix sql migration

--- database/migrations/26/up.sql
+++ database/migrations/26/up.sql
@@ -1,6 +1,6 @@
 PRAGMA user_version = 26;
 PRAGMA foreign_keys = ON;
 
-ALTER TABLE Configuration ADD COLUMN nash_equilibrium_strategy INTEGER;
-ALTER TABLE Configuration ADD COLUMN nash_equilibrium_initialization INTEGER;
-ALTER TABLE Configuration ADD COLUMN max_iteration_nash_equilibrium INTEGER;
+ALTER TABLE Configuration ADD COLUMN nash_equilibrium_strategy INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE Configuration ADD COLUMN nash_equilibrium_initialization INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE Configuration ADD COLUMN max_iteration_nash_equilibrium INTEGER NOT NULL DEFAULT 0;

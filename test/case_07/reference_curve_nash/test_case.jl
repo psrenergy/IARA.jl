906cdec68528f0daa1134b40df632b5a55e07806
gvidigal-psr <gvidigal@psr-inc.com>
2025-09-23 13:57:17 -0300

    Update test case outputs

--- test/case_07/reference_curve_nash/test_case.jl
+++ test/case_07/reference_curve_nash/test_case.jl
@@ -38,7 +38,6 @@ else
         test_only_subperiod_sum = [
             "deficit",
             "hydro_generation",
-            "hydro_minimum_outflow_slack",
             "hydro_om_costs",
             "hydro_turbining",
             "hydro_minimum_outflow_violation_cost_expression",

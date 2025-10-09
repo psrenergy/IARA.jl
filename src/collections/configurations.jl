3b1100727cf7d5cadb66dfc655bf6231457afd79
carolmcs <monteirocarneirosc@gmail.com>
2025-10-09 15:29:55 -0300

    Fix some tests

--- src/collections/configurations.jl
+++ src/collections/configurations.jl
@@ -363,7 +363,7 @@ function initialize!(configurations::Configurations, inputs::AbstractInputs)
         PSRDatabaseSQLite.read_time_series_file(inputs.db, "Configuration", "period_season_map")
 
     configurations.period_season_map =
-        ones(Int, 3, configurations.number_of_periods, configurations.number_of_scenarios)
+        ones(Int, 3, configurations.number_of_scenarios, configurations.number_of_periods)
 
     update_time_series_from_db!(configurations, inputs.db, initial_date_time(inputs))
 

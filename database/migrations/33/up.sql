52844dd4730b56c5e284cea0914fd173791740be
gvidigal-psr <gvidigal@psr-inc.com>
2025-11-11 12:33:09 -0300

    Change min_bid from MWh to MW

--- database/migrations/33/up.sql
+++ database/migrations/33/up.sql
@@ -1,3 +1,3 @@
 PRAGMA user_version = 33;
 
-ALTER TABLE AssetOwner ADD COLUMN minimum_virtual_reservoir_purchase_bid_quantity REAL NOT NULL DEFAULT 0.0;
+ALTER TABLE AssetOwner ADD COLUMN minimum_virtual_reservoir_purchase_bid_quantity_in_mw REAL NOT NULL DEFAULT 0.0;

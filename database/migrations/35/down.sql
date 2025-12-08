1281d8edd5aa137b907d7a00be9f67e591b9fc63
gvidigal-psr <gvidigal@psr-inc.com>
2025-12-08 16:15:05 -0300

    Fix migration bug

--- database/migrations/35/down.sql
+++ database/migrations/35/down.sql
@@ -1,18 +1,15 @@
--- Revert renaming of Supply Function Equilibrium (SFE) parameters from supply_function_equilibrium_* back to reference_curve_nash_*
+PRAGMA user_version = 34;
 
--- Create old columns
 ALTER TABLE Configuration ADD COLUMN reference_curve_nash_extra_bid_quantity REAL DEFAULT 0.0;
 ALTER TABLE Configuration ADD COLUMN reference_curve_nash_tolerance REAL DEFAULT 0.0;
 ALTER TABLE Configuration ADD COLUMN reference_curve_nash_max_iterations INTEGER DEFAULT 0;
 ALTER TABLE Configuration ADD COLUMN reference_curve_nash_max_cost_multiplier REAL DEFAULT 0.0;
 
--- Copy data from new columns back to old columns
 UPDATE Configuration SET reference_curve_nash_extra_bid_quantity = supply_function_equilibrium_extra_bid_quantity;
 UPDATE Configuration SET reference_curve_nash_tolerance = supply_function_equilibrium_tolerance;
 UPDATE Configuration SET reference_curve_nash_max_iterations = supply_function_equilibrium_max_iterations;
 UPDATE Configuration SET reference_curve_nash_max_cost_multiplier = supply_function_equilibrium_max_cost_multiplier;
 
--- Drop new columns
 ALTER TABLE Configuration DROP COLUMN supply_function_equilibrium_extra_bid_quantity;
 ALTER TABLE Configuration DROP COLUMN supply_function_equilibrium_tolerance;
 ALTER TABLE Configuration DROP COLUMN supply_function_equilibrium_max_iterations;
--- database/migrations/35/up.sql
+++ database/migrations/35/up.sql
@@ -1,18 +1,15 @@
--- Rename Supply Function Equilibrium (SFE) parameters from reference_curve_nash_* to supply_function_equilibrium_*
+PRAGMA user_version = 35;
 
--- Create new columns with updated names
 ALTER TABLE Configuration ADD COLUMN supply_function_equilibrium_extra_bid_quantity REAL DEFAULT 0.0;
 ALTER TABLE Configuration ADD COLUMN supply_function_equilibrium_tolerance REAL DEFAULT 0.0;
 ALTER TABLE Configuration ADD COLUMN supply_function_equilibrium_max_iterations INTEGER DEFAULT 0;
 ALTER TABLE Configuration ADD COLUMN supply_function_equilibrium_max_cost_multiplier REAL DEFAULT 0.0;
 
--- Copy data from old columns to new columns
 UPDATE Configuration SET supply_function_equilibrium_extra_bid_quantity = reference_curve_nash_extra_bid_quantity;
 UPDATE Configuration SET supply_function_equilibrium_tolerance = reference_curve_nash_tolerance;
 UPDATE Configuration SET supply_function_equilibrium_max_iterations = reference_curve_nash_max_iterations;
 UPDATE Configuration SET supply_function_equilibrium_max_cost_multiplier = reference_curve_nash_max_cost_multiplier;
 
--- Drop old columns
 ALTER TABLE Configuration DROP COLUMN reference_curve_nash_extra_bid_quantity;
 ALTER TABLE Configuration DROP COLUMN reference_curve_nash_tolerance;
 ALTER TABLE Configuration DROP COLUMN reference_curve_nash_max_iterations;

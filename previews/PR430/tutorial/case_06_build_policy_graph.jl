f40e3bbfe16d5470fe5c9ffc7b2f289d10ee1f65
Documenter.jl <documenter@juliadocs.github.io>
2026-02-05 20:19:59 +0000

    build based on 572e039

---
+++ previews/PR430/tutorial/case_06_build_policy_graph.jl
@@ -0,0 +1,166 @@
+# # Policy Graphs - Building
+
+# > The data for this case is available in the folder [`data/case_6`](https://github.com/psrenergy/IARA.jl/tree/main/docs/src/tutorial/data/case_6)
+
+# The goal is to understand the differences between linear and cyclic policy graphs by running simulations
+# with different numbers of periods. For this tutorial, we will:
+
+# - Compare linear and cyclic policy graphs.
+# - Vary the number of periods (2 and 10).
+# - Link the case to inflow and demand time series files that match the number of periods.
+# - Analyze the results to determine how the optimal solution changes depending on the policy graph type and the number of periods.
+
+# We'll start by importing the necessary packages.
+
+using Dates
+using DataFrames
+using IARA
+; #hide
+
+# We also need to define a directory to store the case.
+const PATH_CASE = joinpath(@__DIR__, "data", "case_6")
+
+# Define conversion factors and key parameters for the study.
+
+## Define basic parameters
+number_of_periods = 2
+number_of_subperiods = 1
+number_of_scenarios = 3
+subperiod_duration_in_hours = [24.0]
+expected_repeats = 5
+cycle_discount_rate = 1 / (expected_repeats - 1)
+; #hide
+
+## Let's define a few conversion factors that we will use later.
+m3_per_second_to_hm3_per_hour = 3600.0 / 1e6
+; #hide
+
+# Using [`IARA.create_study!`](@ref) we can create a new study. 
+
+db = IARA.create_study!(PATH_CASE;
+    number_of_periods = number_of_periods,
+    number_of_scenarios = number_of_scenarios,
+    number_of_subperiods = number_of_subperiods,
+    subperiod_duration_in_hours = subperiod_duration_in_hours,
+    number_of_nodes = number_of_periods,
+    cycle_discount_rate = cycle_discount_rate,
+    policy_graph_type = IARA.Configurations_PolicyGraphType.LINEAR,
+    demand_deficit_cost = 3000.0,
+    cycle_duration_in_hours = 48.0,
+    demand_scenarios_files = IARA.Configurations_UncertaintyScenariosFiles.ONLY_EX_ANTE,
+    inflow_scenarios_files = IARA.Configurations_UncertaintyScenariosFiles.ONLY_EX_ANTE,
+);
+
+# ## Zone and Bus
+
+IARA.add_zone!(db; label = "Zone1")
+IARA.add_bus!(db; label = "Bus1", zone_id = "Zone1")
+
+# ## Demand
+
+IARA.add_demand_unit!(db;
+    label = "Demand1",
+    parameters = DataFrame(;
+        date_time = [DateTime(0)],
+        existing = [1],
+    ),
+    bus_id = "Bus1",
+    max_demand = 3.6,
+)
+
+# ## Physical Elements
+
+# ### Thermal Unit
+
+IARA.add_thermal_unit!(
+    db;
+    label = "Thermal1",
+    parameters = DataFrame(;
+        date_time = [DateTime(0)],
+        existing = [1],
+        max_generation = [25.0],
+        om_cost = [10.0],
+    ),
+    bus_id = "Bus1",
+)
+
+IARA.add_thermal_unit!(
+    db;
+    label = "Thermal2",
+    parameters = DataFrame(;
+        date_time = [DateTime(0)],
+        existing = [1],
+        max_generation = [50.0],
+        om_cost = [20.0],
+    ),
+    bus_id = "Bus1",
+)
+
+# ### Gauging Station
+
+IARA.add_gauging_station!(db;
+    label = "gauging_station",
+)
+
+# ### Hydro Unit
+
+IARA.add_hydro_unit!(db;
+    label = "Hydro1",
+    parameters = DataFrame(;
+        date_time = [DateTime(0)],
+        existing = [1], # 1 = true
+        production_factor = [1.0], # MW/m³/s
+        max_generation = [150.0], # MW
+        max_turbining = [150.0], # m³/s
+        min_volume = [0.0], # hm³
+        max_volume = [100.0], # hm³
+        min_outflow = [0.0], # m³/s
+        om_cost = [0.0], # $/MWh
+    ),
+    initial_volume = 100.0 *
+                     m3_per_second_to_hm3_per_hour *
+                     subperiod_duration_in_hours[1],  # If it is full, it can generate for the whole year # hm³
+    gaugingstation_id = "gauging_station",
+    bus_id = "Bus1",
+)
+
+#  ## Time Series
+
+# ### Loading time series files
+
+# Using a text editor, we have created the following CSV files containing time series information about the demand and solar generation:
+# - `demands.csv`
+# - `inflow.csv`
+
+# Let's take a look at the first lines of each file, using the function [`IARA.time_series_dataframe`](@ref).
+
+# ### Demand:
+
+IARA.time_series_dataframe(joinpath(PATH_CASE, "demands.csv"))
+
+# ### Inflow:
+
+IARA.time_series_dataframe(joinpath(PATH_CASE, "inflow.csv"))
+
+# ### Linking the time series
+
+# Now we need to link the time series with the function [`IARA.link_time_series_to_file`](@ref).
+
+IARA.link_time_series_to_file(
+    db,
+    "DemandUnit";
+    demand_ex_ante = "demands",
+)
+
+IARA.link_time_series_to_file(
+    db,
+    "HydroUnit";
+    inflow_ex_ante = "inflow",
+)
+;
+
+# ## Closing the database
+
+# Now that we have added all the elements and linked the time series files, we can close the database to run the case, with the function [`IARA.close_study!`](@ref).
+
+IARA.close_study!(db)
---
+++ previews/PR430/tutorial/case_06_run_policy_graph.jl
@@ -0,0 +1,209 @@
+# # Policy Graphs - Running
+
+# > The data for this case is available in the folder [`data/case_6`](https://github.com/psrenergy/IARA.jl/tree/main/docs/src/tutorial/data/case_6)
+
+# ## Recap
+# In this tutorial, we explore the differences between two types of policy graphs, linear and cyclic, using a simple model with varying numbers of periods.
+# The model contains one zone, one bus, one demand point, one hydro unit, and one thermal unit.
+# We will analyze how changes in policy graph types affect the decision-making process in centralized operations,
+# focusing on the impact of different configurations of periods.
+
+# We'll start by importing the necessary packages.
+
+using Dates
+using DataFrames
+using IARA
+; #hide
+
+# First, let's create a folder that will contain the execution for each iteration.
+
+const PATH_ORIGINAL = joinpath(@__DIR__, "data", "case_6")
+
+const PATH_EXECUTION = joinpath(@__DIR__, "case_6_execution")
+if !isdir(PATH_EXECUTION)
+    mkdir(PATH_EXECUTION)
+end
+
+# ## Running the Case: Linear Policy Graph with 2 Stages
+
+# Let's create a copy of the original case.
+
+const PATH_LINEAR_2 = joinpath(PATH_EXECUTION, "linear_2")
+if !isdir(PATH_LINEAR_2)
+    mkdir(PATH_LINEAR_2)
+end
+
+cp(PATH_ORIGINAL, PATH_LINEAR_2; force = true);
+
+# In the [previous section](case_06_build_policy_graph.md) we created a case with 2 periods and a linear policy graph.
+# Therefore, we do not need to update the number of periods or the policy graph type.
+
+# Let's begin by running the case with a linear policy graph and 2 periods.
+# This scenario simulates a situation where decision-making happens over two periods with no cyclic behavior.
+
+IARA.train_min_cost(PATH_LINEAR_2)
+
+# ### Analyzing the results
+
+# Here's the graph of the final volume at each period
+
+hydro_final_volume_all =
+    joinpath(PATH_LINEAR_2, "outputs", "hydro_final_volume.csv") # hide
+IARA.custom_plot(
+    hydro_final_volume_all,
+    IARA.PlotTimeSeriesQuantiles;
+    title = "Reservoir Final Volume",
+    agents = ["Hydro1"],
+    period = 1:2,
+)
+
+# For this case, the optimal strategy for the hydro unit is to release water in the last period.
+# This happens because there are no future costs associated with the last period, encouraging full use of available resources.
+
+# ## Running the Case: Linear Policy Graph with 10 Stages
+
+# Next, we increase the number of periods to 10 while keeping the policy graph linear.
+# This allows us to see how a longer planning horizon influences the decision-making process.
+
+const PATH_LINEAR_10 = joinpath(PATH_EXECUTION, "linear_10")
+
+if !isdir(PATH_LINEAR_10)
+    mkdir(PATH_LINEAR_10)
+end
+
+cp(PATH_ORIGINAL, PATH_LINEAR_10; force = true);
+
+# Now we need to update the number of periods to 10.
+
+db = IARA.load_study(PATH_LINEAR_10; read_only = false)
+
+IARA.update_configuration!(
+    db;
+    number_of_periods = 10,
+)
+
+# We will also update the inflow and demand time series files to have 10 periods.
+
+IARA.link_time_series_to_file(
+    db,
+    "DemandUnit";
+    demand_ex_ante = "demands_10_periods",
+)
+
+IARA.link_time_series_to_file(
+    db,
+    "HydroUnit";
+    inflow_ex_ante = "inflow_10_periods",
+)
+
+IARA.close_study!(db)
+
+# Let's run the case.
+
+IARA.train_min_cost(PATH_LINEAR_10)
+
+# ### Analyzing the results
+
+# Here's the graph of the final volume at each period
+
+hydro_final_volume_all =
+    joinpath(PATH_LINEAR_10, "outputs", "hydro_final_volume.csv") # hide
+IARA.custom_plot(
+    hydro_final_volume_all,
+    IARA.PlotTimeSeriesQuantiles;
+    title = "Reservoir Final Volume",
+    agents = ["Hydro1"],
+    period = 1:2,
+)
+
+# With 10 periods, the model accounts for a longer planning horizon, influencing the decision to release water more conservatively in earlier periods.
+# The hydro unit no longer empties the reservoir in a single burst but instead manages water levels more carefully over time, anticipating future periods.
+
+IARA.custom_plot(
+    hydro_final_volume_all,
+    IARA.PlotTimeSeriesQuantiles;
+    title = "Reservoir Final Volume",
+    agents = ["Hydro1"],
+    period = 3:10,
+)
+
+# As the planning horizon progresses, the model becomes more aggressive in its approach,
+# depleting the reservoir by the end of the fourth year.
+
+# ## Running the Case: Cyclic Policy Graph with 2 Stages
+
+# Now, we will switch the policy graph to cyclic, which assumes that the decision-making process repeats over time.
+
+const PATH_CYCLIC_2 = joinpath(PATH_EXECUTION, "cyclic_2")
+
+if !isdir(PATH_CYCLIC_2)
+    mkdir(PATH_CYCLIC_2)
+end
+
+cp(PATH_ORIGINAL, PATH_CYCLIC_2; force = true);
+
+# Now we need to update the policy graph type to `CYCLIC_WITH_NULL_ROOT`.
+
+db = IARA.load_study(PATH_CYCLIC_2; read_only = false)
+
+IARA.update_configuration!(
+    db;
+    number_of_periods = 2,
+    policy_graph_type = IARA.Configurations_PolicyGraphType.CYCLIC_WITH_NULL_ROOT,
+)
+
+# We will also update the inflow and demand time series files to have 2 periods, and replace the period dimension with season, as required for cyclic cases.
+
+IARA.link_time_series_to_file(
+    db,
+    "DemandUnit";
+    demand_ex_ante = "demands_by_season",
+)
+
+IARA.link_time_series_to_file(
+    db,
+    "HydroUnit";
+    inflow_ex_ante = "inflow_by_season",
+)
+;
+
+IARA.close_study!(db)
+; #hide
+
+# Let's run the case.
+
+IARA.train_min_cost(PATH_CYCLIC_2)
+
+# ### Analyzing the results
+
+# Here's the graph of the final volume at each period
+
+hydro_final_volume_all =
+    joinpath(PATH_CYCLIC_2, "outputs", "hydro_final_volume.csv") # hide
+IARA.custom_plot(
+    hydro_final_volume_all,
+    IARA.PlotTimeSeriesQuantiles;
+    title = "Reservoir Final Volume",
+    agents = ["Hydro1"],
+    period = 1:2,
+)
+
+# With a cyclic policy graph, the decision-making process becomes more dynamic.
+# The hydro unit is now planning with the expectation that the periods will repeat, creating an incentive
+# to maintain a certain level of water in the reservoir.
+
+# ## Conclusion
+
+# Through these simulations, we observe significant differences between linear and cyclic policy graphs.
+# In the 2-period linear setup, the model chooses an aggressive strategy at the end of the first year,
+# since there is no incentive to conserve water for future periods.
+# This short-term focus leads to a rapid depletion of the reservoir.
+
+# With 10 periods in the linear policy graph, the model's approach becomes more conservative, water
+# levels are managed more carefully during the early years, reflecting a longer-term outlook.
+# However, the reservoir is still emptied by the end of the fourth year,
+# indicating a gradual shift toward resource depletion as the planning horizon progresses.
+
+# In contrast, the cyclic policy graph with 2 periods adds a dynamic element to the decision-making process.
+# Here, the hydro unit balances the immediate need for power generation with the understanding that the
+# periods will repeat, encouraging more sustainable water management across cycles.

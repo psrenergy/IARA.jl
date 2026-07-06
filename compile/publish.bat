80fb52ef193ab861eb72d6cfc5ff62b60308ebe3
raphasampaio <raphaelaraujosampaio@gmail.com>
2026-07-06 15:29:41 -0300

    Updates

--- compile/publish.bat
+++ compile/publish.bat
@@ -2,4 +2,4 @@
 
 SET BASE_PATH=%~dp0
 
-CALL julia +1.11.7 --project=%BASE_PATH% %BASE_PATH%\publish.jl %*
+CALL julia +1.11.2 --project=%BASE_PATH% %BASE_PATH%\publish.jl %*
--- compile/publish.sh
+++ compile/publish.sh
@@ -3,4 +3,4 @@ set -e
 
 BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-julia +1.11.7 --project=$BASE_PATH $BASE_PATH/publish.jl $@
+julia +1.11.2 --project=$BASE_PATH $BASE_PATH/publish.jl $@
